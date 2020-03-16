13ae88003d46fd22612f2b1d89b2938d8ea05139
Kristin Evans <kevans0625@gmail.com>
2020-03-16 15:08:14 -0500

    updated seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -3,24 +3,36 @@ CREATE DATABASE cms_DB;
 USE cms_DB;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT default 0,
-  PRIMARY KEY (id)
+  department_id INT,
+  CONSTRAINT `fk_role_department`
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id) ON UPDATE CASCADE ON DELETE RESTRICT 
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT default 0,
+  role_id INT,
   manager_id INT default 0,
-  PRIMARY KEY (id)
+  CONSTRAINT `fk_employee_role`
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id) ON UPDATE CASCADE ON DELETE RESTRICT 
 );
+
+INSERT INTO department (name) 
+VALUES ("marketing");
+
+INSERT INTO role(title, salary)
+VALUES ("coordinator", 15.75);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Kristin", "Evans");
