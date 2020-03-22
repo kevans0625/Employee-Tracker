96dfdaf3dc9aaf9d1ac5f349dd042e85615521b6
Kris <Home@Kristins-MacBook-Pro.local>
2020-03-21 19:38:56 -0500

    separated seeds from schemas.

--- seeds.sql
+++ seeds.sql
@@ -1,38 +1,32 @@
-DROP DATABASE IF EXISTS sms_DB;
-CREATE DATABASE cms_DB;
 USE cms_DB;
 
-CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Executive");
+INSERT INTO department (name) VALUES ("Member Services");
+INSERT INTO department (name) VALUES ("Education");
+INSERT INTO department (name) VALUES ("Accouting");
+INSERT INTO department (name) VALUES ("Creative Services");
+INSERT INTO department (name) VALUES ("Human Resources");
 
-CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT,
-  CONSTRAINT `fk_role_department`
-  FOREIGN KEY (department_id) 
-  REFERENCES department(id) ON UPDATE CASCADE ON DELETE RESTRICT 
-);
+INSERT INTO role(title, salary, department_id) VALUES ("Marketing Coordinator", 15.75,1);
+INSERT INTO role(title, salary, department_id) VALUES ("Office Pet", 15.75,3);
+INSERT INTO role(title, salary, department_id) VALUES ("Digital Specalist", 15.75,2);
+INSERT INTO role(title, salary, department_id) VALUES ("Lawyer", 15.75,3);
+INSERT INTO role(title, salary, department_id) VALUES ("Cinematographer", 15.75,7);
+INSERT INTO role(title, salary, department_id) VALUES ("Manager", 15.75,3);
+INSERT INTO role(title, salary, department_id) VALUES ("Education Director", 15.75,5);
+INSERT INTO role(title, salary, department_id) VALUES ("Payroll Assistant", 15.75,6);
+INSERT INTO role(title, salary, department_id) VALUES ("HR Coordinator", 15.75,8);
+INSERT INTO role(title, salary, department_id) VALUES ("Recruiter", 15.75,8);
 
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT default 0,
-  CONSTRAINT `fk_employee_role`
-  FOREIGN KEY (role_id) 
-  REFERENCES role(id) ON UPDATE CASCADE ON DELETE RESTRICT 
-);
-
-INSERT INTO department (name) 
-VALUES ("marketing");
-
-INSERT INTO role(title, salary)
-VALUES ("coordinator", 15.75);
-
-INSERT INTO employee (first_name, last_name)
-VALUES ("Kristin", "Evans");
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kristin", "Evans",3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Shahan", "McClendon",4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Surcie", "McClendon",2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Travanti", "Robinson",5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Ares", "Robinson",2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Makhyla", "Johnson",)6;
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kahaleel", "Johnson",7);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kameron", "Brooks",8);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Detrix", "Brooks",9);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Eyan", "Smith",10);
